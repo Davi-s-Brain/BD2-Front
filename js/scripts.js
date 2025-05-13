82c8f5f4e6de202a5437ae57c15744f4ef249b96
Davi Batista <dsouza5@uolinc.com>
2025-05-13 16:16:15 -0300

    Adicionando HTML, CSS e JS esqueletos

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuLateral = document.querySelector(".menu-lateral");
+  const secoesMenu = menuLateral.querySelectorAll(".secao-menu");
+
+  secoesMenu.forEach((secao) => {
+    secao.addEventListener("click", function () {
+      secoesMenu.forEach((s) => s.classList.remove("ativo"));
+      this.classList.add("ativo");
+
+      const secaoId = this.getAttribute("data-secao");
+      console.log(`Seção "${secaoId}" foi clicada.`);
+
+      const todasSecoesProdutos = document.querySelectorAll(".lista-produtos");
+      todasSecoesProdutos.forEach((sec) => (sec.style.display = "none"));
+      const secaoAlvo = document.getElementById(`${secaoId}-secao`);
+      if (secaoAlvo) {
+        secaoAlvo.style.display = "block";
+      } else if (secaoId === "lancamentos") {
+        document.getElementById("lancamentos-secao").style.display = "block";
+      }
+    });
+  });
+
+  function atualizarPontos(novosPontos) {
+    const elementoPontos = document.getElementById("pontos");
+    if (elementoPontos) {
+      elementoPontos.textContent = novosPontos;
+    }
+  }
+});
